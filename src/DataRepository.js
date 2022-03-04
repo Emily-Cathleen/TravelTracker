7ca41baf3d0e8fe797ed0fd09583ed194d005e9f
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-04 18:02:20 -0500

    Create dataRepo

---
+++ src/DataRepository.js
@@ -0,0 +1,13 @@
+import dayjs from 'dayjs'
+
+class DataRepository {
+  constructor(data) {
+    this.travelers = data.travelers;
+    this.trips = data.trips;
+    this.destinations = data.destinations;
+  }
+
+calculateTotalSpentThisYear()
+
+
+}
