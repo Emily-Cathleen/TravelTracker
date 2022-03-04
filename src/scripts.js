788987a51fb8c9da1a4bb7177fdb508d02b5de41
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-04 18:03:14 -0500

    Add function

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,21 @@
-
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import Traveler from "./Traveler";
+import Destination from "./Destination";
+import Trip from "./Trip";
 
-console.log('This is the JavaScript entry file - your code begins here.');
-console.log("TEST MERGE")
+import {
+  fetchUserData,
+  fetchTripsData,
+  fetchDestinationData,
+} from "./apiCalls.js";
+
+//GLOBALS
+let data = {}
+let currentTraveler;
+
+
+const instantiateTraveler = (id) => {
+  currentTraveler = new Traveler
+}
