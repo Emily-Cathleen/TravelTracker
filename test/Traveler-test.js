48f2492315f6ff1f728e6d64d2792a9f918f04d1
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-04 16:05:21 -0500

    Add tests for ID

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,15 +1,20 @@
 import { expect } from "chai";
 import Traveler from "../src/Traveler";
+import travelerTestData from "../src/sample-data/traveler-test-data";
+
+
 
 describe("Traveler", () => {
 
   let traveler1;
   let traveler2;
+  let traveler3;
 
   beforeEach(() => {
 
-    traveler1 = new Traveler();
-    traveler2 = new Traveler();
+    traveler1 = new Traveler(travelerTestData[0]);
+    traveler2 = new Traveler(travelerTestData[1]);
+    traveler3 = new Traveler(travelerTestData[2]);
   });
 
   it("should be a function", function () {
@@ -18,4 +23,16 @@ describe("Traveler", () => {
 
   it("should be an instance of Traveler", () => {
     expect(traveler1).to.be.an.instanceOf(Traveler);
+    expect(traveler2).to.be.an.instanceOf(Traveler);
+    expect(traveler3).to.be.an.instanceOf(Traveler);
+  });
+
+  it("should have an id", () => {
+    expect(traveler1.id).to.equal(1);
+    expect(traveler2.id).to.equal(2);
+    expect(traveler3.id).to.equal(3);
+  });
+
+
+
   });
