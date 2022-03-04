d2f40c04ef46cab638acc6688da8aecb523f1e88
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-04 16:15:06 -0500

    Add api calls

---
+++ src/apiCalls.js
@@ -0,0 +1,37 @@
+import domUpdates from "./domUpdates";
+
+//FETCH calls
+
+
+const fetchUserData = () => {
+  return fetch("http://localhost:3001/api/v1/travelers")
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+      domUpdates.showError(
+        "Sorry, we were unable to retrieve your data. Please contact @nikseif to file a complaint"
+      );
+    });
+};
+
+const fetchDestinationData = () => {
+  return fetch("http://localhost:3001/api/v1/destinations")
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+      domUpdates.showError(
+        "Sorry, we were unable to retrieve your data. Please contact @sertmer to file a complaint"
+      );
+    });
+};
+
+const fetchTripsData = () => {
+  return fetch("http://localhost:3001/api/v1/trips")
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+      domUpdates.showError(
+        "Sorry, we were unable to retrieve your data. Please contact @sertmer to file a complaint"
+      );
+    });
+};
