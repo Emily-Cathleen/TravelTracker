ff8126a87d72e4f7d433af650f7136718df2dd19
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-04 18:03:08 -0500

    Add updated function

---
+++ src/domUpdates.js
@@ -0,0 +1,19 @@
+ 
+const welcome = document.getElementById("welcome");
+
+
+
+//DOM MANIPULATION FUNCTIONS
+
+const getRandomIndex = (array) => {
+  return Math.floor(Math.random() * array.length);
+};
+
+greetUser(currentTraver) {
+    welcome.innerText = `Welcome, ${currentTraveler.getFirstName()}`;
+  },
+
+
+
+
+export default domUpdates;
