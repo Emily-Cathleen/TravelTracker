9a17fd9266010fcb5385785ca5e9d47aa63cb24a
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-04 19:03:36 -0500

    Created functions to get traveler name to display on Dom successfully!

--- src/scripts.js
+++ src/scripts.js
@@ -6,16 +6,47 @@ import Destination from "./Destination";
 import Trip from "./Trip";
 
 import {
-  fetchUserData,
-  fetchTripsData,
+  fetchTravelerData,
+  fetchTripData,
   fetchDestinationData,
 } from "./apiCalls.js";
 
 //GLOBALS
 let data = {}
-let currentTraveler;
+let travelers;
+let trips;
+let destinations;
+let randomIndex;
 
+//QUERY SELECTORS
 
-const instantiateTraveler = (id) => {
-  currentTraveler = new Traveler
-}
+const welcome = document.getElementById("welcome");
+
+//FUNCTIONS
+
+const fetchAllData = () => {
+  Promise.all([
+    fetchTravelerData(),
+    fetchTripData(),
+    fetchDestinationData(),
+  ]).then((data) => parseAllData(data))
+};
+
+const parseAllData = (data) => {
+  console.log("DAATT", data)
+  travelers = data[0].travelers.map(traveler => new Traveler(traveler));
+  // trips = data[1].trips.map(trip => new Trip(trip));
+  // destinations = data[2].destinations.map(destination => new Destination(destination));
+  getRandomIndex(travelers)
+};
+
+const getRandomIndex = (array) => {
+  randomIndex = Math.floor(Math.random() * array.length);
+  greetUser(randomIndex)
+};
+
+const greetUser = (traveler) => {
+    welcome.innerText = `Adventure Awaits, ${travelers[randomIndex].getFirstName()}`;
+  };
+
+window.addEventListener('load', fetchAllData);
