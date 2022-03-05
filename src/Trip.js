38f9261892450e16fd7993a807c0a7d29dec12c4
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-04 19:13:59 -0500

    Change file name to match other sample data files

---
+++ src/Trip.js
@@ -0,0 +1,11 @@
+class Trip {
+  constructor(tripData) {
+    this.id = tripData.id;
+    this.userID = tripData.userID;
+    this.travelers = tripData.travelers;
+    this.date = tripData.date;
+    this.duration = tripData.duration;
+    this.status = tripData.status;
+    this.suggestedActivities = tripData.suggestedActivities;
+  }
+}
