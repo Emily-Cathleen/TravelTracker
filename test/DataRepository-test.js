9ba25bf191d50ac7d5ec70769a19d84a024b1143
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-04 20:49:13 -0500

    Add describe block

--- test/DataRepository-test.js
+++ test/DataRepository-test.js
@@ -1,8 +1,34 @@
 import chai from 'chai';
 const expect = chai.expect;
+import DataRepository from "../src/DataRepository";
+import Traveler from "../src/Traveler";
+import Trip from "../src/Trip";
+import Destination from "../src/Destination";
+import destinationTestData from "../src/sample-data/destination-test-data";
+import tripTestData from "../src/sample-data/trip-test-data";
+import travelerTestData from "../src/sample-data/traveler-test-data";
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
-  });
-});
+
+describe("DataRepository", () => {
+  let dataRepository1;
+  let dataRepositoryTestData;
+  let travelerData;
+  let tripData;
+  let destinationData;
+
+  beforeEach(() => {
+
+    travelerData = travelerTestData.map((traveler) => new Traveler(traveler));
+    tripData = tripTestData.map((trip) => new Trip(trip));
+    destinationData = destinationTestData.map((destination) => new Destination(destination));
+
+    dataRepositoryTestData = {
+      travelers: travelerData,
+      trips: tripData,
+      destinations: destinationData
+    }
+
+    dataRepository1 = new DataRepository(dataRepositoryTestData)
+  })
+
+})
