8851fac54465be3e91ae12de4bc6fef820f048a1
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-04 19:02:22 -0500

    Change fetch calls to correct order

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,26 @@
-import domUpdates from "./domUpdates";
+// import domUpdates from "./domUpdates";
 
 //FETCH calls
 
 
-const fetchUserData = () => {
+const fetchTravelerData = () => {
   return fetch("http://localhost:3001/api/v1/travelers")
     .then((response) => response.json())
     .catch((error) => {
       console.log(error);
       domUpdates.showError(
         "Sorry, we were unable to retrieve your data. Please contact @nikseif to file a complaint"
+      );
+    });
+};
+
+const fetchTripData = () => {
+  return fetch("http://localhost:3001/api/v1/trips")
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+      domUpdates.showError(
+        "Sorry, we were unable to retrieve your data. Please contact @sertmer to file a complaint"
       );
     });
 };
@@ -25,13 +36,4 @@ const fetchDestinationData = () => {
     });
 };
 
-const fetchTripsData = () => {
-  return fetch("http://localhost:3001/api/v1/trips")
-    .then((response) => response.json())
-    .catch((error) => {
-      console.log(error);
-      domUpdates.showError(
-        "Sorry, we were unable to retrieve your data. Please contact @sertmer to file a complaint"
-      );
-    });
-};
+export {fetchTravelerData, fetchTripData, fetchDestinationData}
