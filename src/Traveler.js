2c0a20a5b588a03531f6fdeaf6d6a242223bf264
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-05 17:02:21 -0500

    Add to trip class

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,14 +1,34 @@
+import Trip from './Trip.js';
+
 class Traveler {
   constructor(travelerData) {
     this.id = travelerData.id;
     this.name = travelerData.name;
     this.travelerType = travelerData.travelerType;
+    this.allTrips = [];
   }
 
   getFirstName() {
     const firstName = this.name.split(" ")[0];
     return firstName
   }
+
+ //  getAllTrips(allTrips, allDestinations) {
+ //   let filteredTrips = allTrips.filter(trip => trip.userID === this.id);
+ //   let currentDestination;
+ //   filteredTrips.forEach(trip => {
+ //     allDestinations.forEach(location => {
+ //       if (trip.destinationID === location.id) {
+ //         currentDestination = location;
+ //         this.allTrips.push(new Trip(trip, currentDestination));
+ //       }
+ //     })
+ //   })
+ //   return this.allTrips
+ // }
+
+
+
 }
 
 
