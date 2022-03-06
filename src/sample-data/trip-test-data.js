431fa35f50f71e0a534f68dfe8a709cc7a313a1d
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-06 16:03:22 -0500

    Fix method

--- src/sample-data/trip-test-data.js
+++ src/sample-data/trip-test-data.js
@@ -10,6 +10,26 @@ status: "approved",
 suggestedActivities: [ ]
 },
 {
+id: 46,
+userID: 44,
+destinationID: 33,
+travelers: 2,
+date: "2020/08/24",
+duration: 11,
+status: "approved",
+suggestedActivities: [ ]
+},
+{
+id: 202,
+userID: 44,
+destinationID: 15,
+travelers: 3,
+date: "2022/08/08",
+duration: 7,
+status: "approved",
+suggestedActivities: [ ]
+},
+{
 id: 2,
 userID: 35,
 destinationID: 25,
