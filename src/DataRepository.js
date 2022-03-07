5b347bc21613139212d6919814746cdc94774adb
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-06 22:18:59 -0500

    Create methods and refactor entire function

--- src/DataRepository.js
+++ src/DataRepository.js
@@ -5,16 +5,27 @@ import Destination from "./Destination";
 class DataRepository {
   constructor(data) {
     this.travelers = data.travelers;
-    this.currentTraveler;
+    this.currentTraveler = data.currentTraveler;
     this.trips = data.trips;
     this.destinations = data.destinations;
     this.date;
   }
 
+  getCurrentTraveler(id) {
+    const travelerInfo = this.travelers.find(traveler => traveler.id === id)
+    this.currentTraveler = travelerInfo
+    return travelerInfo
+  }
+
+  getFirstName() {
+    const firstName = this.currentTraveler.name.split(" ")[0];
+    return firstName
+  }
+
   createNewTraveler(travelerData) {
     this.currentTraveler = new Traveler(travelerData);
     this.currentTraveler.allTrips = this.getTravelerTrips(this.currentTraveler.id);
-    this.sortTravelerTrips();
+    // this.sortTravelerTrips();
   }
 
   getTravelerTrips(travelerID) {
@@ -26,46 +37,67 @@ class DataRepository {
     return tripsByTravelerID
   }
 
-  currentDate(trip) {
-    let endDate = trip.date.setDate(trip.duration)
-     return new Date(trip.date).valueOf() <= new Date().valueOf() &&
-     new Date(endDate).valueOf() >= new Date().valueOf()
+getPendingTrips() {
+  const pending = this.trips.filter(trip => {
+    if(trip.status === "pending") {
+    this.currentTraveler.pendingTrips.push(trip)
   }
-
-  isAfterToday(trip) {
-    return new Date(trip.date).valueOf() > new Date().valueOf();
-  }
-
-  isBeforeToday(trip) {
-    let endDate = trip.date.setDate(trip.duration)
-    return new Date(endDate).valueOf() < new Date().valueOf();
-  }
-  //if my endDate < in miliseconds myCurrentDay, then this is going to be true
-  //otherwise,this is going to be false
-
-sortTravelerTrips() {
-  this.currentTraveler.allTrips.forEach(trip => {
-    trip.date = trip.date.replaceAll('/', '-');
-    if(currentDate(trip)) {
-      this.currentTraveler.presentTrips.push(trip)
-    } else if (isAfterToday(trip)){
-      this.currentTraveler.upcomingTrips.push(trip)
-    } else if (isBeforeToday(trip)){
-      this.currentTraveler.pastTrips.push(trip)
-    } else if (trip.status === "pending") {
-      this.currentTraveler.pendingTrips.push(trip)
-    }
-  })
+})
+  return pending
 }
 
-  // calculateTotalSpentThisYear() {
-  //   const totalLodging = this.destinations.estimatedLodgingCostPerDay * this.duration;
-  //   const totalFlight = this.destinations.estimatedFlightCostPerPerson * this.travelerCount;
-  //   const tripCost = totalLodging + totalFlight;
-  //   const costWithAgentFee = tripCost + (tripCost * .10);
-  //     this.tripCost = costWithAgentFee;
-  //     return costWithAgentFee;
-  //   }
+ //  getPastTrips(currentDate) {
+ //   const pastTrips = this.trips.filter(trip => {
+ //     const tripDate = new Date(`${trip.date}`).getTime();
+ //     if (currentDate > tripDate) {
+ //       return trip;
+ //     }
+ //   });
+ //   traveler.pastTrips = pastTrips;
+ //   return pastTrips;
+ // }
+
+
+ // calculateTotalSpentThisYear() {
+ //   const totalLodging = this.destinations.estimatedLodgingCostPerDay * trips.duration;
+ //   const totalFlight = this.destinations.estimatedFlightCostPerPerson * this.travelerCount;
+ //   const tripCost = totalLodging + totalFlight;
+ //   const costWithAgentFee = tripCost + (tripCost * .10);
+ //     this.tripCost = costWithAgentFee;
+ //     return costWithAgentFee;
+ //   }
+
+
+
+//   currentDate(trip) {
+//     let endDate = trip.date.setDate(trip.duration)
+//      return new Date(trip.date).valueOf() <= new Date().valueOf() &&
+//      new Date(endDate).valueOf() >= new Date().valueOf()
+//   }
+//
+//   isAfterToday(trip) {
+//     return new Date(trip.date).valueOf() > new Date().valueOf();
+//   }
+//
+//   isBeforeToday(trip) {
+//     let endDate = trip.date.setDate(trip.duration)
+//     return new Date(endDate).valueOf() < new Date().valueOf();
+//   }
+//
+// sortTravelerTrips() {
+//   this.currentTraveler.allTrips.forEach(trip => {
+//     trip.date = trip.date.replaceAll('/', '-');
+//     if(currentDate(trip)) {
+//       this.currentTraveler.presentTrips.push(trip)
+//     } else if (isAfterToday(trip)){
+//       this.currentTraveler.upcomingTrips.push(trip)
+//     } else if (isBeforeToday(trip)){
+//       this.currentTraveler.pastTrips.push(trip)
+//     } else if (trip.status === "pending") {
+//       this.currentTraveler.pendingTrips.push(trip)
+//     }
+//   })
+// }
 
 }
 
