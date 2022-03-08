a8c8a2ea88ab3215dd8299b00e391701e435e147
Emily Corcoran <cathleencorcoran@gmail.com>
2022-03-08 17:41:07 -0500

    Create a method to add the estiamted cost for a user generated trip

--- src/DataRepository.js
+++ src/DataRepository.js
@@ -34,6 +34,7 @@ class DataRepository {
     const tripsByTravelerID = this.trips.filter(trip => {
       if (trip.userID === travelerID) {
         this.currentTraveler.allTrips.push(trip)
+        // console.log("LOOK", this.currentTraveler.allTrips)
         return trip
       }
     })
@@ -81,9 +82,9 @@ class DataRepository {
       let tripYear = parseInt(trip.date.split('/')[0])
       return tripYear === 2022
     })
-     const findPrice = userTrips.reduce((acc, currentTrip) => {
+    const findPrice = userTrips.reduce((acc, currentTrip) => {
       this.destinations.forEach(destination => {
-        if(destination.id === currentTrip.destinationID) {
+        if (destination.id === currentTrip.destinationID) {
           acc += destination.estimatedLodgingCostPerDay * currentTrip.duration;
           acc += destination.estimatedFlightCostPerPerson * currentTrip.travelers;
         }
@@ -91,8 +92,24 @@ class DataRepository {
       return acc
     }, 0)
     return findPrice * 1.1;
-    }
+  }
 
+  getDestinationById(id) {
+    const result = this.destinations.find(destination => {
+      if (destination.id === id) {
+        return destination
+      }
+    })
+    return result
+  }
+
+  getEstimatedTripCost(trip, destination) {
+    console.log("LOOKHERE", destination)
+    const tripTotal = (destination.estimatedLodgingCostPerDay * trip.duration) +
+      (destination.estimatedFlightCostPerPerson * trip.travelers)
+    const fee = tripTotal / 0.1
+    return fee + tripTotal
+  }
 
 };
 
